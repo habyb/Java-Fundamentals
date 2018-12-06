80638cf54261449f697fbb3b70bece637e4b1fd7
Habyb.Fernandes <habybfernandes@gmail.com>
2018-12-06 15:46:19 -0200

    Added: VarType Project

---
+++ VarType/src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		var x2 = 20;
+//		x2 = 29.0; // Error: Type mismatch: cannot convert from double to int. Java assumes 'int' at compile time
+		
+		boolean y = false;
+		var y2 = true;
+		
+//		var z; // Cannot use 'var' on variable without initializer
+		
+	}
+}
