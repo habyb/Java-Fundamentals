4f2723383c74db362f8aa601af3c2cc36c587b23
Habyb.Fernandes <habybfernandes@gmail.com>
2018-12-05 22:03:45 -0200

    Added: Variables Project

---
+++ Variables/src/Aplicacao.java
@@ -0,0 +1,36 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+//		int number;
+//		number = 50;
+		
+//		int number = 20;
+//		int number2 = number + 10;
+//		
+//		number = number - 1;
+//		number -= 1;
+//		number--;
+//		
+//		number = number * 2;
+//		number *= 2;
+//		
+//		System.out.println(number);
+//		System.out.println(number2);
+//		
+//		boolean x = true;
+//		System.out.println(x);
+		
+//		long l = 10L + 5;
+//		double d = 5 * 2.0;
+//		System.out.println(l);
+//		System.out.println(d);
+		
+//		double d = 0.0 / 0;
+//		System.out.println(d); // NaN - Not a Number
+		
+		double d = 10.0 / 0;
+		System.out.println(d); // Infinity
+	}
+}
